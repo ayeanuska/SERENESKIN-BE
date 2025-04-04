ccf0769f1872326438700e651579d767cb27f95f
anuska <anuskasth2022@gmail.com>
2025-04-04 19:54:02 +1100

    refresh token created that will be stored in userTable

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,15 +1,16 @@
 import jwt from 'jsonwebtoken'
 import { CreateNewSession } from '../model/session/SessionModel'
 // generate accessJWT
-export const createAccessJWt = async (payload)=>{
+export const createAccessJWt = async (email)=>{
 
 //create token
 
-    const token =jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:"20min"})
+    const token =jwt.sign({email}, process.env.JWT_SECRET, {expiresIn:"20min"})
     //store in session table / db.
 
     const obj = {
         token,
+        association: email,
         expiresIn: new Date(Date.now() + 20 * 60 * 1000),//20 min
     
     }
@@ -24,4 +25,13 @@ export const createAccessJWt = async (payload)=>{
 
 
 //generate refreshJWT
+export const creteRefreshJwt = async (email)=>{
+    // create token 
+    const token = jwt.sign({email}, process.env.REFRESH_JWT_SECRET, {expiresIn:"30days"})
+
+    // storing in user table 
+   const user = await updateUser({email}, {refreshJWT})
+   return user?._id refreshJWT : null;
+}
+
 //decode refreshJWT
