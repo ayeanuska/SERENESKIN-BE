6288dd33e0353811b4ae57f493b5a84ade6dc787
anuska <anuskasth2022@gmail.com>
2025-04-04 19:34:40 +1100

    jwt access token created

---
+++ src/utils/jwt.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+import { CreateNewSession } from '../model/session/SessionModel'
+// generate accessJWT
+export const createAccessJWt = async (payload)=>{
+
+//create token
+
+    const token =jwt.sign(payload, "secret", {expiresIn:"20min"})
+    //store in session table 
+
+    const obj = {
+        token,
+        expiresIn: new Date(Date.now() + 20 * 60 * 1000),//20 min
+    
+    }
+
+
+    const newSession = await CreateNewSession(obj),
+    return newSession?._id ? token : null,
+}
+
+//decode accessJWT
+
+
+
+//generate refreshJWT
+//decode refreshJWT
