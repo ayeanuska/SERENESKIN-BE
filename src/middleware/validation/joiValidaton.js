fe4807bde8873bbbdf67bc6028fd5aeb61828bc7
anuska <anuskasth2022@gmail.com>
2025-05-05 22:15:17 +1000

    Joi for register

--- src/middleware/validation/joiValidaton.js
+++ src/middleware/validation/joiValidaton.js
@@ -1,23 +1,30 @@
 import Joi from "joi";
+import { responseClient } from "../responseClient";
 
 export const validateData = (req, res, next) => {
-  try {
-    // create schema or rules
+  // create schema or rules
 
-    const schema = Joi.object({
-      fName: Joi.string().required(),
-      lName: Joi.string().required(),
-      email: Joi.string().email({ minDomainSegments: 2 }).required(),
-      phone: Joi.number().required(),
-      password: Joi.string().required(),
+  const schema = Joi.object({
+    fName: Joi.string().required(),
+    lName: Joi.string().required(),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    phone: Joi.number().required(),
+    password: Joi.string().required(),
+  });
+
+  // pass your data , req.body , to the schema .. this will spit out response
+  const value = schema.validate(req.body);
+  console.log(value);
+
+  if (value.error) {
+    return responseClient({
+      req,
+      res,
+      message: value.error.message,
+      statusCode: 400,
     });
-
-    // pass your data , req.body , to the schema .. this will spit out response
-    const value = schema.validate(req.body);
-    console.log(value);
-
-    // IF pass go next() or respond error
-  } catch (error) {
-    console.log(error);
   }
+  next();
+
+  // IF pass go next() or respond error
 };
